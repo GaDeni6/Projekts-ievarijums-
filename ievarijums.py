8950a8e68810abb9ecb7e4ec5e251a01f19354a4
GaDeni6 <artursbelousss@gmail.com>
2026-09-21 09:45:41 +0300

    evgenija kods

--- ievarijums.py
+++ ievarijums.py
@@ -1,27 +1,11 @@
-cukura_cena=input("Ievadi cukura cenu: ") 
+receptes_numurs=input("Ievadiet receptes numuru: \n1) 1 kg abolu = 300 gr. cukura\n2) 1 kg abolu = 500 gr. cukura\n ")
+cukura_cena=float(input("Ievadi cukura cenu: "))
 aboli_kg = float(input("Ievadi, cik abolu tev ir (kg): "))
-print(f"Par cukuru tu samaksi {cukura_cena*aboli_kg*0.7} eiro")
-
-receptes_numurs = int(
-input(
-"Ievadiet receptes numuru:\n1) 1 kg ābolu = 300 gr. cukura\n2) 1 kg ābolu = 500 gr. cukura\n"
-)
-)
-
-cukura_cena = float(input("Ievadi cukura cenu: "))
-abolus_kg = float(input("Ievadi, cik ābolu tev ir (kg): "))
-
-
-if receptes_numurs == 1:
-    cukurs_uz_kg = 0.3 
+if receptes_numurs == "1":
+    izmaksas = cukura_cena*aboli_kg*0.3
 else:
-    cukurs_uz_kg = 0.5 
-
-
-kopeja_cena = abolus_kg * cukurs_uz_kg * cukura_cena
-
-
-print(f"Par cukuru tu samaksāsi {kopeja_cena} eiro")
+    izmaksas = cukura_cena*aboli_kg*0.5
+print(f"Par cukuru tu samaksasi {izmaksas} eiro")
 
 
 
