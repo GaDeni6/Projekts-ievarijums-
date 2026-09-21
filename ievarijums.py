e933b8082ba97beda54515dd7c66ce5623e14f69
GaDeni6 <artursbelousss@gmail.com>
2026-09-21 08:54:24 +0300

    Programmas pirma versija

---
+++ ievarijums.py
@@ -0,0 +1,11 @@
+cukura_cena=input("Ievadi cukura cenu: ") 
+aboli_kg = float(input("Ievadi, cik abolu tev ir (kg): "))
+print(f"Par cukuru tu samaksi {cukura_cena*aboli_kg*0.7} eiro")
+
+
+
+
+
+
+
+
